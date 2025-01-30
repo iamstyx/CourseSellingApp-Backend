8685fed638bb8c4eff7854eb1d6fa9f3cdc1c8c1
iamstyx <sushantt.verma@gmail.com>
2025-01-30 16:54:11 +0530

    Added DB Schema and Admin Routes

--- db.js
+++ db.js
@@ -1,22 +1,51 @@
-const {Schema} = require("mongoose")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+dotenv.config();
 
-const userSchema = Schema({
+console.log("Connected To MongoDB")
+mongoose.connect(process.env.Mongo_URL)
+
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: {type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email: {type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+
 
 });
 
-const adminSchema = Schema({
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model(" purchase", purchaseSchema);
 
-});
-
-const courseSchema = Schema({
-
-});
-
-const purchaseSchema = Schema({
-
-});
-
-const userModel = mongoose.Model("user", userSchema);
-const adminModel = mongoose.Model("admin", adminSchema);
-const courseModel = mongoose.Model("course", courseSchema);
-const purchaseModel = mongoose.Model(" purchase", purchaseSchema);
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
